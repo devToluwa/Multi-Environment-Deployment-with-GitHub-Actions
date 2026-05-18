d2c8249ba2ef0ffd7dc5beac2ce3763b1f22659b
Toluwa Oloba <toluwadev@outlook.com>
2026-05-18 16:26:16 +0100

    test iam

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "github_actions" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" = "repo:devToluwa/Multi-Environment-Deployment-With-GitHub-Actions*"
         }
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
